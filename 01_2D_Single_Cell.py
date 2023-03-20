a319ac11fa72aad0fd8148250cc99c19905c2de1
EmrahDURSUN <emrahdrsn@gmail.com>
2023-03-20 09:41:02 +0100

    Single Cell simplified organized comments added for better undertanding, unneccasry paramters are removed

--- 01_2D_Single_Cell.py
+++ 01_2D_Single_Cell.py
@@ -1,98 +1,78 @@
 # 01 2D Single Cell
 # Create two-dimensional fluid region
-# Emrah Dursun. 23/02/203. 
-# Python Script, API Version = V22 Beta
+# Emrah Dursun. 20/03/2023.
+
 from SpaceClaim.Api.V22.Geometry import Point
 import math
 
 Selection.Empty()
-#ClearAll()
-#Layers.DeleteEmpties()
+ClearAll()
+Layers.DeleteEmpties()
 
 # Get Input Parameters
-per=Parameters.Periodicity/2
 periodicity=Parameters.Periodicity
-subThick=Parameters.SubsThickness
-hCr1=Parameters.ChromiumForAdhesion
-hAu=Parameters.GoldLayer
-#hCr2=Parameters.ChromiumForSurfacePassivationm
 radius=Parameters.Radius
 width=Parameters.Width 
 angle=Parameters.Angle
 oangle=Parameters.OpeningAngle
-hEtch=Parameters.EtchHeight
-dEtch=Parameters.EtchDepth
-dnaT = Parameters.DNA
-opRegion = Parameters.OpenRegion
 channelWidth=Parameters.Channel_Width
-numXElements=Parameters.X_direction_repeat
-numZElements=Parameters.Z_direction_repeat
 # End Parameters
   
 # Create Component and activate it
 singleCellTwoDComp = ComponentHelper.CreateAtRoot("2D Single Cell")
 ComponentHelper.SetActive(singleCellTwoDComp)
  
-# Beginning of Surface Creation
-######################################################################
+# Allocate points and create a surface operation
+def allocatePoints(distanceAwayFromOrigin, rotationBasedOrigin):
+    # Create DatumPoint and rotate it
+    selectedPoints = Selection.Create(DatumPointCreator.Create(Point.Create(0, MM(distanceAwayFromOrigin), 0)).CreatedPoint)
+    move = Move.Rotate(selectedPoints, Line.Create(Point.Origin, Direction.DirZ), rotationBasedOrigin, MoveOptions())
     
-def pointAllocateFirst( distP, angP):
-    seloc =Selection.Create(DatumPointCreator.Create(Point.Create(0, MM(distP), 0)).CreatedPoint)
-    move = Move.Rotate(seloc, Line.Create(Point.Origin, Direction.DirZ),  angP, MoveOptions())
+'''' pointsDict is a way to keep track of the DatumPoints that were created in pointAllocateFirst
+so that they can be used later in surfaceCutOperation to define the boundaries of the resulting PlanarBody.
+'''
+
+# Create ring surface with given parameters
+def createRingSurface(ring, nearPoint, farPoint, rotation):
+
+    # Allocate points to create surface
+    pointsDict = {1: [(nearPoint, rotation), (nearPoint, -rotation), (farPoint, rotation), (farPoint, -rotation)],
+                   2: [(nearPoint, rotation), (nearPoint, -rotation), (farPoint, rotation), (farPoint, -rotation)]}
+
+    for point in pointsDict[ring]:
+        allocatePoints(point[0], point[1])
+
+    # Get positions of DatumPoints
+    start1 = singleCellTwoDComp.Content.DatumPoints[0].Position
+    end1 = singleCellTwoDComp.Content.DatumPoints[1].Position
+    start2 = singleCellTwoDComp.Content.DatumPoints[2].Position
+    end2 = singleCellTwoDComp.Content.DatumPoints[3].Position
     
-def pointAllocateSecond(distP,angP):
-    seloc =Selection.Create(DatumPointCreator.Create(Point.Create(0, MM(distP), 0)).CreatedPoint)
-    move = Move.Rotate(seloc, Line.Create(Point.Origin, Direction.DirZ), angP, MoveOptions())
-    
-def surfaceCutOperation(ring, nearPoint, farPoint, rotation):
-    if (ring == 1 or ring == 3):
-        pointAllocateFirst( nearPoint, rotation)
-        pointAllocateFirst( nearPoint, -rotation)
-        pointAllocateFirst( farPoint, rotation)
-        pointAllocateFirst( farPoint, -rotation)
-    if (ring == 2 or ring ==  4 or ring ==  5):
-        pointAllocateSecond( nearPoint, rotation)
-        pointAllocateSecond( nearPoint, -rotation)
-        pointAllocateSecond( farPoint, rotation)
-        pointAllocateSecond( farPoint, -rotation)
-       
-    start1 =(singleCellTwoDComp.Content.DatumPoints[0]).Position
-    end1 = (singleCellTwoDComp.Content.DatumPoints[1]).Position
-    start2 =(singleCellTwoDComp.Content.DatumPoints[2]).Position
-    end2 = (singleCellTwoDComp.Content.DatumPoints[3]).Position
-    
+    # Delete DatumPoints
     while singleCellTwoDComp.Content.DatumPoints.Count > 0:
         singleCellTwoDComp.Content.DatumPoints[0].Delete()
-        
+    
+    # Create curves and surface
     curves = List[ITrimmedCurve]()
-    curves.Add(CurveSegment.CreateArc(Point.Origin,start1,end1, -Direction.DirZ))
-    curves.Add(CurveSegment.CreateArc(Point.Origin,start2,end2, -Direction.DirZ))
-    curves.Add(CurveSegment.Create(start1,start2))
-    curves.Add(CurveSegment.Create(end1,end2))
+    curves.Add(CurveSegment.CreateArc(Point.Origin, start1, end1, -Direction.DirZ))
+    curves.Add(CurveSegment.CreateArc(Point.Origin, start2, end2, -Direction.DirZ))
+    curves.Add(CurveSegment.Create(start1, start2))
+    curves.Add(CurveSegment.Create(end1, end2))
         
     designResult = PlanarBody.Create(Plane.PlaneXY, curves)
-    designBody1 = designResult.CreatedBody.SetName('Surface' + ring.ToString() )
+    designBody1 = designResult.CreatedBody.SetName('Surface' + str(ring))
   
-######################################################################
-# Create Operations
-surfaceCutOperation( 1, radius,             radius+width,                    DEG(180) +angle)  #Cut Up
-surfaceCutOperation( 2, radius,             radius+width,                    DEG(180) - angle - oangle)  #Cut Up
-#######################################################################
+# Call Create ring surfaces with given parameters
+createRingSurface(1, radius, radius + width, DEG(180) + angle)
+createRingSurface(2, radius, radius + width, DEG(180) - angle - oangle)
 
-###############################################################
 
+# Create a rectangular channel surface and set its name
 originRectang=Point.Create(-channelWidth/2, -periodicity/2,0)
 result=RectangularSurface.Create( channelWidth, periodicity, originRectang)
 body = result.CreatedBody.SetName('Channel')
 
-## Merge Bodies
-#targets = Selection.CreateByNames('Channel','surface1')
-#result = Combine.Merge(targets)
-#targets = Selection.CreateByNames('surface2','surface1')
-#result = Combine.Merge(targets)
-## EndBlock
-#seloc =  Selection.CreateByNames('surface1')
-
+# Merge all surfaces and rename
 num = singleCellTwoDComp.Content.Bodies.Count-1
 t=0
 while t < num:
@@ -100,13 +80,9 @@ while t < num:
     selectionOthers = BodySelection.Create(singleCellTwoDComp.Content.Bodies[num-1-t])
     result = Combine.Merge( selectionTarget , selectionOthers)
     t+=1
-
 RenameObject.Execute(BodySelection.Create(singleCellTwoDComp.Content.Bodies[0]), 'Combined_Surface')
-    
-#move = Move.Rotate(seloc, Line.Create(Point.Origin, Direction.DirX), DEG(90), MoveOptions())
 
-#######################################################################
-
+# Ending Arrangements
 Selection.Clear()
 ComponentHelper.SetRootActive()
 mode = ViewHelper.ViewProjection.Top
